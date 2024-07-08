286319c3c896cf23e813af5e3a9c50f22db3798c
adbenitez <asieldbenitez@gmail.com>
2024-07-08 16:40:47 +0200

    improve realtime.ts
    * send data in a more efficient way
    * use received time for messages instead of sent time
    * add disconnect() method to RealTime class

--- src/realtime.ts
+++ src/realtime.ts
@@ -1,5 +1,10 @@
-import { createEncoder, toUint8Array, writeVarString } from "lib0/encoding";
-import { createDecoder, readVarString } from "lib0/decoding";
+import {
+  createEncoder,
+  toUint8Array,
+  writeVarString,
+  writeVarUint,
+} from "lib0/encoding";
+import { createDecoder, readVarString, readVarUint } from "lib0/decoding";
 import { User, Msg } from "./types";
 
 type PresenceCallback = (users: Record<string, User>) => void;
@@ -10,17 +15,6 @@ const messageTypes = {
   Normal: 2,
 };
 
-function encode(obj: any): Uint8Array {
-  const encoder = createEncoder();
-  writeVarString(encoder, JSON.stringify(obj));
-  return toUint8Array(encoder);
-}
-
-function decode(data: Uint8Array): any {
-  const decoder = createDecoder(data);
-  return JSON.parse(readVarString(decoder));
-}
-
 export default class RealTime {
   users: Record<string, User>;
   messages: Msg[];
@@ -28,6 +22,7 @@ export default class RealTime {
   timeout: number;
   _onPresenceChanged: PresenceCallback;
   _onMessagesChanged: MessagesCallback;
+  _interval: number;
 
   constructor({
     onPresenceChanged,
@@ -46,34 +41,52 @@ export default class RealTime {
 
     const self = this;
     this.channel.setListener((data: Uint8Array) => {
-      const update = data.subarray(1);
-      if (data[0] === messageTypes.Presence) {
-        const user = decode(update);
-        if (user.time === -1) {
-          delete self.users[user.name];
+      const decoder = createDecoder(data);
+      const mType = readVarUint(decoder);
+      if (mType === messageTypes.Presence) {
+        const name = readVarString(decoder);
+        const online = readVarUint(decoder);
+        if (online) {
+          self.users[name] = { name, lastSeen: new Date().valueOf() };
         } else {
-          self.users[user.name] = {
-            name: user.name,
-            lastSeen: new Date().valueOf(),
-          };
+          delete self.users[name];
         }
         self._onPresenceChanged(self.users);
-      } else if (data[0] === messageTypes.Normal) {
-        self._addMessage(decode(update));
+      } else if (mType === messageTypes.Normal) {
+        self._addMessage(readVarString(decoder), readVarString(decoder));
+      } else {
+        console.error("Unexpected MessageType: " + mType);
       }
     });
-    setInterval(() => this._sync(), tick * 1000);
+    this._interval = window.setInterval(() => this._sync(), tick * 1000);
+  }
+
+  disconnect() {
+    clearInterval(this._interval);
+    this._sendPresence(0);
+  }
+
+  sendMessage(text: string) {
+    if (!text) return;
+    const sender = window.webxdc.selfName;
+    const encoder = createEncoder();
+    writeVarUint(encoder, messageTypes.Normal);
+    writeVarString(encoder, sender);
+    writeVarString(encoder, text);
+    this.channel.send(toUint8Array(encoder));
+    this._addMessage(sender, text);
+  }
+
+  _sendPresence(state: number) {
+    const encoder = createEncoder();
+    writeVarUint(encoder, messageTypes.Presence);
+    writeVarString(encoder, window.webxdc.selfName);
+    writeVarUint(encoder, state);
+    this.channel.send(toUint8Array(encoder));
   }
 
   _sync() {
-    const state = {
-      name: window.webxdc.selfName,
-    };
-    const update = encode(state);
-    const data = new Uint8Array(update.length + 1);
-    data[0] = messageTypes.Presence;
-    data.set(update, 1);
-    this.channel.send(data);
+    this._sendPresence(1);
 
     let usersChanged = false;
     const self = this;
@@ -89,26 +102,15 @@ export default class RealTime {
     }
   }
 
-  _addMessage(msg: Msg) {
+  _addMessage(sender: string, text: string) {
     if (this.messages.length >= 500) {
       this.messages.shift();
     }
-    this.messages.push(msg);
-    this._onMessagesChanged(this.messages);
-  }
-
-  sendMessage(text: string) {
-    if (!text) return;
-    const msg: Msg = {
-      senderName: window.webxdc.selfName,
+    this.messages.push({
+      senderName: sender,
       time: new Date().valueOf(),
       text: text,
-    };
-    const update = encode(msg);
-    const data = new Uint8Array(update.length + 1);
-    data[0] = messageTypes.Normal;
-    data.set(update, 1);
-    this.channel.send(data);
-    this._addMessage(msg);
+    });
+    this._onMessagesChanged(this.messages);
   }
 }
